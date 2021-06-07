4dae21ac7a7afb9676abd70284d2d5c3562c2a06
Brett Sutton <bsutton@noojee.com.au>
2021-06-07 20:38:52 +1000

    Revised the install so that it installs into an existing OS path as overwriting the version in pub-cache was causing future pub gets to fail. The error was: Failed to decode data using encoding 'utf-8', path = '~/.pub-cache/bin/dswitch'

--- bin/dswitch_install.dart
+++ bin/dswitch_install.dart
@@ -3,7 +3,41 @@ import 'dart:io';
 import 'package:dcli/dcli.dart';
 import 'package:dswitch/dswitch.dart';
 
-void main() {
+void main(List<String> args) {
+  final parser = ArgParser();
+  parser.addFlag('verbose',
+      abbr: 'v', defaultsTo: false, help: 'Dump verbose logging information');
+
+  parser.addOption('stage2', abbr: '2', help: 'Stage 2');
+
+  final parsed = parser.parse(args);
+  Settings().setVerbose(enabled: parsed['verbose'] as bool);
+
+  if (!parsed.wasParsed('stage2')) {
+    runStage1();
+  } else {
+    var pathToDSwitch = parsed['stage2'] as String;
+    runStage2(pathToDSwitch);
+    exit(0);
+  }
+
+  print(orange('dswitch is ready to run'));
+}
+
+void runStage1() {
+  if (!Shell.current.isPrivilegedUser) {
+    if (Platform.isWindows) {
+      print(red('Please run dswitch_install with Administrative privileges.'));
+      exit(1);
+    }
+  }
+
+  // 'bash'.run;
+  // Shell.current.releasePrivileges();
+
+  // env[PubCache.envVarPubCache] =
+  //     join(rootPath, 'home', Shell.current.loggedInUser, '.pub_cache');
+
   // build the path to the copy of bin/dswitch.dart in the pub cache.
   final pathToDSwitch = join(
     DartProject.fromCache('dswitch', packageVersion).pathToProjectRoot,
@@ -11,20 +45,58 @@ void main() {
 
   if (!exists(pathToDSwitch)) {
     printerr(
-        "Could not find dswitch in pub cache. Please run 'dart pub global activate dswitch' and try again.");
+        "Could not find dswitch_install in pub cache. Please run 'dart pub global activate dswitch' and try again.");
     exit(1);
   }
 
   withTempDir((compileDir) {
     copyTree(pathToDSwitch, compileDir);
 
-    final script = DartScript.fromFile(join(compileDir, 'bin', 'dswitch.dart'));
-    print('Compiling dswitch so it will run independant of your dart version.');
+    final installScript =
+        DartScript.fromFile(join(compileDir, 'bin', 'dswitch_install.dart'));
+    print('');
+    print(blue('Compiling dswitch_install'));
     DartSdk().runPubGet(compileDir, progress: Progress.devNull());
-    script.compile(workingDirectory: compileDir);
+    installScript.compile(workingDirectory: compileDir);
 
-    /// replace the pub-cache script with a compiled version.
-    copy(script.pathToExe, PubCache().pathToBin, overwrite: true);
-  });
-  print(orange('DSwitch is ready to run'));
+    print('');
+    print(blue('Compiling dswitch.'));
+    final dswitchScript =
+        DartScript.fromFile(join(compileDir, 'bin', 'dswitch.dart'));
+    dswitchScript.compile(workingDirectory: compileDir);
+
+    // /// duplicate the script as we may not be able to copy ourselves
+    // final duplicate =
+    //     join(compileDir, '${script.basename}2${extension(script.basename)}');
+    // copy(script.pathToExe, duplicate, overwrite: true);
+
+    if (!Platform.isWindows) {
+      print(green(
+          'Please provide your sudo password so we can install dswitch into your PATH'));
+    }
+    print('');
+    start('${installScript.pathToExe} --stage2=${dswitchScript.pathToExe}',
+        privileged: true);
+  }, keep: true);
+}
+
+/// In stage 2 we are running from a compiled exe as a privilged user.
+void runStage2(String pathToDSwitch) {
+  String target;
+  if (Platform.isWindows) {
+    target = join(
+        env['USERPROFILE']!, 'AppData', 'Local', 'Microsoft', 'WindowsApps');
+  } else {
+    target = '/usr/bin';
+  }
+
+  if (!exists(pathToDSwitch)) {
+    printerr(
+        "Could not find dswitch in pub cache. Please run 'dart pub global activate dswitch' and try again.");
+    exit(1);
+  }
+
+  print(blue('Installing dswitch into $target.'));
+  copy(pathToDSwitch, target, overwrite: true);
+  print('');
 }
