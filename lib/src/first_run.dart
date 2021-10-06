6765238ee8cb492c73ccd3892417edd64427c747
Brett Sutton <bsutton@noojee.com.au>
2021-10-07 10:13:33 +1100

    upgraded to dcli 1.9.2

--- lib/src/first_run.dart
+++ lib/src/first_run.dart
@@ -16,7 +16,7 @@ void firstRun() {
 void checkIsFullyInstalled() {
   if (!isCurrentVersionInstalled) {
     print(red(
-        'A new version of dswitch has been activated. Pleasd run dswitch_install and then try again.'));
+        'A new version of dswitch has been activated. Please run dswitch_install and then try again.'));
     exit(1);
   }
 
